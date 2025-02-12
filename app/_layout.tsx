5e9b788f3cf0d3889407c7eebda9c60d429a9eda
Huy Dang <xhuydng@gmail.com>
2025-02-12 02:15:40 -0500

    removed root's header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,12 @@ import { Stack } from 'expo-router';
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: 'Home' }} />
+      <Stack.Screen name="(tabs)" options={{ 
+        headerShown: false, // Remove header, all header control is in (tabs)
+        headerLeft: () => <></>, // To prevent having a "back" button from the error page
+       }} />
       <Stack.Screen name="about" options={{ title: 'About' }} />
+      <Stack.Screen name = "+not-found" options={{ title: 'Error Page'}} />
     </Stack>
   );
 }
